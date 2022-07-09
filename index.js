8c8a9227fc3463d46b2fdd091dda9dcfae4d29d3
koriscs <koriscs@gmail.com>
2022-07-09 17:30:08 +0200

    Deploying Heroku

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ require('dotenv').config();
 const helmet = require('helmet');
 const isProduction = process.env.NODE_ENV === 'production';
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 //import passport middleware
 require('./server/middlewear/passport');
-
+const publicPath = path.join(__dirname, 'client', 'public');
 
 //initialize middlewears
 app.use(helmet());
@@ -19,6 +20,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors(origin = {origin:isProduction ? process.env.HEROKU_URL : process.env.CLIENT_URL, credentials: true }));
 app.options('*', cors(origin));
+app.use(express.static(publicPath));
 app.use(passport.initialize());
 
 //import routes
@@ -27,7 +29,9 @@ const productRouter = require('./server/routes/products');
 const cartRouter = require('./server/routes/cart');
 
 //initialize routes
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
 app.use('/auth', authRouter);
 app.use('/products', productRouter);
 app.use('/cart', cartRouter);
