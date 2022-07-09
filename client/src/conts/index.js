09739133b6c5ab37e450d13f2a4d153eab2844b0
koriscs <koriscs@gmail.com>
2022-07-09 15:25:59 +0200

    Deploying Heroku

--- client/src/conts/index.js
+++ client/src/conts/index.js
@@ -1,4 +1,5 @@
-const baseURL =  process.env.HEROKU_URL || "http://localhost:3000" ;
+//const baseURL =  process.env.HEROKU_URL || "http://localhost:3000" ;
+const baseURL = "https://fullstack-webshop.herokuapp.com";
 
 module.exports = {
     baseURL: baseURL
