c9e63d8da353f979148a0f2758fa3b562fff91cf
koriscs <koriscs@gmail.com>
2022-07-18 17:54:09 +0200

    Deploy on Heroku

--- middlewear/passport.js
+++ middlewear/passport.js
@@ -40,7 +40,7 @@ passport.use(
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL: "https://fullstack-webshop.herokuapp.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const { rows } = await pool.query("SELECT * FROM customers WHERE google_id = $1;",[profile.id])
