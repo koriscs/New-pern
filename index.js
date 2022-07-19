85da0bd5bc53f9acfa2b28c42f4762c0668c6321
koriscs <koriscs@gmail.com>
2022-07-19 09:58:12 +0200

    Deploy on Heroku

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ require('./middlewear/passport');
 
 //initialize middlewears
 app.use(helmet());
-app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      "script-src": ["'self'", "https://js.stripe.com","https://checkout.stripe.com"],
-      "img-src": ["'self'", "https: data:"],
-      "connect-src":["'self","https://api.stripe.com"],
-      "frame-src":["'self","https://js.stripe.com","https://hooks.stripe.com"]
-    }
-  })
-)
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     useDefaults: true,
+//     directives: {
+//       "script-src": ["'self'", "https://js.stripe.com","https://checkout.stripe.com"],
+//       "img-src": ["'self'", "https: data:"],
+//       "connect-src":["'self","https://api.stripe.com"],
+//       "frame-src":["'self","https://js.stripe.com","https://hooks.stripe.com"]
+//     }
+//   })
+// )
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
